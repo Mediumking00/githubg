63311dfbb343406ba554c52ce662cc2b0d018c1a
Yotsaphon <jew2121@gmail.com>
2019-01-15 00:31:53 +0700

    Circle area

--- tz1.cpp
+++ tz1.cpp
@@ -1,10 +1,23 @@
 #include<iostream>
 using namespace std;
+double findcirclearea(double r)
+{
+  double A =3.14*r*r;
+  return A;
+}
 
 int main()
 {
-  int x=5;
-    cout<<"Print"<<x<<"\n";
+  double A1 = findcirclearea(1);
+  cout<<"A1 = "<<A1<<'\n';
+
+  double r2 = 2;
+  double A2 = findcirclearea(r2);
+  cout << "A2 =" <<A2<< '\n';
+
+  cout << "A3 =" <<findcirclearea(3)<< '\n';
+
+
 
 
   return 0;
